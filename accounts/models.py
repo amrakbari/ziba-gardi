a9b7b0b2ed57aec2c5518a0f5762cd381d6f95ef
amrakbari <amrakbaridev@gmail.com>
2023-02-10 23:29:18 +0330

    feat: created custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
+
 # Create your models here.
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
