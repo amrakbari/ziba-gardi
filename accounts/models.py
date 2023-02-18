8ac50834b25383b7b79dff207beb70c59e92dde5
amrakbari <amrakbaridev@gmail.com>
2023-02-18 21:46:26 +0330

    feat: added model Role

--- accounts/models.py
+++ accounts/models.py
@@ -2,11 +2,17 @@ from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
 
+class Role(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(auto_now=True)
+
+
 # Create your models here.
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
-
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     objects = UserManager()
