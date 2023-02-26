5e5907eeafd3b466faceb73d22ff30f07f6cfdea
amrakbari <amrakbaridev@gmail.com>
2023-02-26 16:45:48 +0330

    feat: added model - StylistProfile

--- store_management/models.py
+++ store_management/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from accounts.models import CustomUser, Address
+
+
+class StylistProfile(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
 
 # Create your models here.
