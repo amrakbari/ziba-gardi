0e165a36cdc9cead18f6de5d037e1e262a720ff3
amrakbari <amrakbaridev@gmail.com>
2023-02-26 17:13:01 +0330

    feat: add new model and changed and minor change on some of them

--- store_management/models.py
+++ store_management/models.py
@@ -1,17 +1,27 @@
+from datetime import datetime
+
 from django.db import models
 from accounts.models import CustomUser, Address
 
 
 class StylistProfile(models.Model):
+    title = models.CharField(max_length=255)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(auto_now=True)
 
 
 class Store(models.Model):
     stylist = models.ForeignKey(StylistProfile, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(auto_now=True)
 
 
 class UsualUserProfile(models.Model):
+    title = models.CharField(max_length=255)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(auto_now=True)
 
