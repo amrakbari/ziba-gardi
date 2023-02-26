97e5dc80f29e714388973e8ed0d0a359ba112cf0
amrakbari <amrakbaridev@gmail.com>
2023-02-26 16:46:21 +0330

    feat: created new model Store

--- store_management/models.py
+++ store_management/models.py
@@ -6,4 +6,8 @@ class StylistProfile(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
 
-# Create your models here.
+class Store(models.Model):
+    stylist = models.ForeignKey(StylistProfile, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=255)
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True)
+
