c637910c8e232909b971791a33b69e74cecd8fbe
amrakbari <amrakbaridev@gmail.com>
2023-03-02 15:02:20 +0330

    feat: new model created and appointment rang validation performs

--- store_management/models.py
+++ store_management/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 from django.db import models
+from django.db.models import Q
+
 from accounts.models import CustomUser, Address
 
 
@@ -25,3 +27,19 @@ class UsualUserProfile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(auto_now=True)
 
+
+class Appointment(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    user = models.ForeignKey(UsualUserProfile, on_delete=models.CASCADE, null=True, default=None)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        try:
+            self.objects.get(
+                Q(start_datetime__range=(self.start_datetime, self.end_datetime)) |
+                Q(end_datetime__range=(self.start_datetime, self.end_datetime)) |
+                Q(start_datetime__lt=self.start_datetime, end_datetime__gt=self.end_datetime)
+            )
+        except Appointment.DoesNotExist:
+            super(Appointment, self).save(*args, **kwargs)
