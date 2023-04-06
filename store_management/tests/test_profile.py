2231469d7f77b27698fedf4cd9d3950509d1483d
amrakbari <amrakbaridev@gmail.com>
2023-04-06 15:34:24 +0330

    test: sdded new tests for updating profile

--- store_management/tests/test_profile.py
+++ store_management/tests/test_profile.py
@@ -42,3 +42,41 @@ class TestCreateProfile:
         })
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+class TestUpdateProfile:
+    def test_if_successful_return_200_and_response_matches_put(self, authenticated_client):
+        user = baker.make(CustomUser)
+        api_client = authenticated_client(user)
+
+        response = api_client.put(path=fr'/store/stylist-profiles/', data={
+            'role': 'US',
+            'birth_date': '2020-11-01T00:00:00',
+        })
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json().get('birth_date') == '2020-11-01T00:00:00'
+
+    def test_if_successful_return_200_and_response_matches_patch(self, authenticated_client):
+        user = baker.make(CustomUser)
+        api_client = authenticated_client(user)
+
+        response = api_client.patch(path=fr'/store/stylist-profiles/', data={
+            'birth_date': '2020-11-01T00:00:00',
+        })
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json().get('birth_date') == '2020-11-01T00:00:00'
+
+    def test_if_not_authenticated_return_401(self, authenticated_client):
+        user = baker.make(CustomUser)
+        api_client = authenticated_client(user)
+
+        response = api_client.patch(path=fr'/store/stylist-profiles/', data={
+            'birth_date': '2020-11-01T00:00:00',
+        })
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
