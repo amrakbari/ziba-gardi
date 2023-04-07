2f7190988817ff845cf48b25225ec588fcb756d6
amrakbari <amrakbaridev@gmail.com>
2023-04-07 14:14:21 +0330

    feat: implemented customized action to get current user profile

--- store_management/views.py
+++ store_management/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, mixins, permissions
+from rest_framework import viewsets, mixins, permissions, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from store_management.models import UserProfile
@@ -15,5 +16,12 @@ class CreateUpdateRetrieveProfile(mixins.CreateModelMixin,
     def get_queryset(self):
         return UserProfile.objects.filter(user_id=self.request.user.id)
 
-    def get_object(self):
-        return get_object_or_404(UserProfile)
+    @action(
+        methods=['GET'],
+        detail=False,
+    )
+    def get_current_user_profile(self, request):
+        instance = get_object_or_404(self.get_queryset())
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
