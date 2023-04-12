68816dc25bf9af8eeda10e3a962384d66f6f6903
amrakbari <amrakbaridev@gmail.com>
2023-04-12 15:16:58 +0330

    test: tests for store partial update completed

--- store_management/tests/test_store.py
+++ store_management/tests/test_store.py
@@ -154,25 +154,118 @@ class TestUpdateStorePut:
         store = baker.make(Store, user=user, address=address1)
         sample_data = {
             'title': 'test update',
-            'address': address2
+            'address': address2.id
         }
 
         response = api_client.put(path=fr'/accounts/stores/{store.id}/', data=sample_data)
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_store_id_is_not_for_the_current_user_return_404(self, authenticated_client): ...
+    def test_if_store_id_is_not_for_the_current_user_return_404(self, authenticated_client):
+        user1 = baker.make(CustomUser)
+        user2 = baker.make(CustomUser)
+        user1_address1 = baker.make(Address, user=user1)
+        user1_address2 = baker.make(Address, user=user1)
+        user2_address = baker.make(Address, user=user2)
+        user1_profile = baker.make(UserProfile, user=user1)
+        user2_profile = baker.make(UserProfile, user=user2)
+        user1_store = baker.make(Store, user=user1, address=user1_address1)
+        user2_store = baker.make(Store, user=user2, address=user2_address)
+        api_client = authenticated_client(user1)
 
-    def test_if_address_id_is_not_for_the_current_user_return_404(self, authenticated_client): ...
+        sample_data = {
+            'title': 'test update',
+            'address': user1_address2.id
+        }
+        response = api_client.put(path=fr'/accounts/stores/{user2_store.id}/', data=sample_data)
 
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_if_address_id_is_not_for_the_current_user_return_404(self, authenticated_client):
+        user1 = baker.make(CustomUser)
+        user2 = baker.make(CustomUser)
+        user1_address = baker.make(Address, user=user1)
+        user2_address = baker.make(Address, user=user2)
+        user1_profile = baker.make(UserProfile, user=user1)
+        user2_profile = baker.make(UserProfile, user=user2)
+        user1_store = baker.make(Store, user=user1, address=user1_address)
+        user2_store = baker.make(Store, user=user2, address=user2_address)
+        api_client = authenticated_client(user1)
+
+        sample_data = {
+            'title': 'test update',
+            'address': user2_address.id
+        }
+        response = api_client.put(path=fr'/accounts/stores/{user2_store.id}/', data=sample_data)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
 
 @pytest.mark.django_db
 class TestUpdateStorePatch:
-    def test_if_successful_return_200_and_response_matches(self, authenticated_client): ...
+    def test_if_successful_return_200_and_response_matches(self, authenticated_client):
+        user = baker.make(CustomUser)
+        address1 = baker.make(Address, user=user)
+        address2 = baker.make(Address, user=user)
+        store = baker.make(Store, user=user, address=address1)
 
-    def test_if_not_authenticated_return_401(self, api_client): ...
+        sample_data = {
+            'address': address2.id
+        }
+        api_client = authenticated_client(user)
+        response = api_client.patch(path=fr'/accounts/stores/{store.id}/', data=sample_data)
 
-    def test_if_store_id_is_not_for_the_current_user(self, authenticated_client): ...
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json().get('address') == sample_data.get('address')
+
+    def test_if_not_authenticated_return_401(self, api_client):
+        user = baker.make(CustomUser)
+        address1 = baker.make(Address, user=user)
+        address2 = baker.make(Address, user=user)
+        store = baker.make(Store, user=user, address=address1)
+        sample_data = {
+            'address': address2.id
+        }
+
+        response = api_client.patch(path=fr'/accounts/stores/{store.id}/', data=sample_data)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_store_id_is_not_for_the_current_user_return_404(self, authenticated_client):
+        user1 = baker.make(CustomUser)
+        user2 = baker.make(CustomUser)
+        user1_address1 = baker.make(Address, user=user1)
+        user1_address2 = baker.make(Address, user=user1)
+        user2_address = baker.make(Address, user=user2)
+        user1_profile = baker.make(UserProfile, user=user1)
+        user2_profile = baker.make(UserProfile, user=user2)
+        user1_store = baker.make(Store, user=user1, address=user1_address1)
+        user2_store = baker.make(Store, user=user2, address=user2_address)
+        api_client = authenticated_client(user1)
+
+        sample_data = {
+            'title': 'test update',
+        }
+        response = api_client.patch(path=fr'/accounts/stores/{user2_store.id}/', data=sample_data)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_if_address_id_is_not_for_the_current_user_return_404(self, authenticated_client):
+        user1 = baker.make(CustomUser)
+        user2 = baker.make(CustomUser)
+        user1_address = baker.make(Address, user=user1)
+        user2_address = baker.make(Address, user=user2)
+        user1_profile = baker.make(UserProfile, user=user1)
+        user2_profile = baker.make(UserProfile, user=user2)
+        user1_store = baker.make(Store, user=user1, address=user1_address)
+        user2_store = baker.make(Store, user=user2, address=user2_address)
+        api_client = authenticated_client(user1)
+
+        sample_data = {
+            'address': user2_address.id
+        }
+        response = api_client.patch(path=fr'/accounts/stores/{user1_store.id}/', data=sample_data)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 @pytest.mark.django_db
